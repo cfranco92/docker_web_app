39c60a4b2ac5d7d961d561f66b964b378638a906
Cristian Franco <cfrancobedoya@gmail.com>
2021-09-10 20:44:20 -0500

    Adding text

---
+++ FirstContainer/server.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const express = require("express");
+const http = require('http');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json())
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: true, }));
+app.get("/", (req, res) => {
+    res.json({
+        info: `Esta es la primera aplicación a contenerizar expuesta por el puerto ${app.get('port')}`
+    })
+})
+
+http.createServer(app).listen(app.get('port'), '0.0.0.0', () => {
+    console.log(`Running Express Server Listening on port ${app.get('port')}`)
+})
